573ba06917b2ca40cb72ec90189e71e6eedf0121
Mike Guijarro <a01612042@itesm.mx>
2022-08-11 13:02:29 -0500

    add route protection

--- base/views.py
+++ base/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
 
 def landing(request):
     return render(request, 'base/landing.html')
 
+def logout(request):
+    logout(request)
+    return redirect('home')
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     return render(request, 'base/login.html')
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     return render(request, 'base/signup.html')
 
 
